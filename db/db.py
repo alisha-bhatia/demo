5cabda862e10fdfdcaef4093a6784de4f55c77f2
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2021-11-16 17:28:17 -0500

    Update db.py
    Edited file to pass tests.
     - Ally

--- db/db.py
+++ db/db.py
@@ -10,3 +10,17 @@ def fetch_pets():
     A function to return all pets in the data store.
     """
     return {"tigers": 2, "lions": 3, "zebras": 1}
+
+
+
+def create_cuser(name, demographic, age, categories, location):
+    """
+    A function that will save a user to the data base
+    """
+    user = {"name": name,
+            "age": age,
+            "demographic": demographic,
+            "Interest Categories": categories,
+            "location": location}
+    create = json.dumps(user)
+    print(create)
